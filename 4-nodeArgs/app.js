ce3026ad310214334bd7968e32751dfeec72870f
Mumbi Francis Kinyanjui <kinya.francis.mumbi@gmail.com>
2022-03-09 15:31:16 +0300

    New trial commit

---
+++ 4-nodeArgs/app.js
@@ -0,0 +1,12 @@
+
+// process.argv.forEach((val, index) => {
+//     console.log(`${index} : ${val}`)
+// })
+
+// const args = process.argv.slice(2)
+
+// console.log(args)
+
+const args = require('minimist')(process.argv.slice(2))
+
+console.log(args.name)
---
+++ 5-cliOutput/app.js
@@ -0,0 +1,105 @@
+// const x = 'x'
+// const y = 'y'
+
+// console.log(x , y)
+
+// console.log('My name is %s, and  I have %d eyes', 'Mumbi', 2)
+
+// console.log('%i', 100.45)  100
+
+// console.log('%i', 100.56)  100
+
+// console.log('%o', Number)
+
+// console.clear()
+
+/*
+    Counting elements
+*/
+
+// const x = 1
+// const y = 2
+// const z = 3
+
+// console.count(
+//     "The value of x is and has been checked .. how many times?"
+// )
+
+// console.count(
+//     "The value of x is and has been checked .. how many times?"
+// )
+
+// console.count(
+//     'The value of y is' + y +
+//     'and has been checked .. how many times?'
+// )
+
+// console.log("Hello")
+
+// console.count("Hello")
+
+// const oranges = ['orange', 'orange']
+// const apples = ['just one apple']
+
+// oranges.forEach(fruit => {
+//     console.count(fruit)
+// })
+
+// apples.forEach(fruit => {
+//     console.count(fruit)
+// })
+
+/*
+    Reset Counting
+*/
+
+// const oranges = ['orange', 'orange', 'orange']
+
+// oranges.forEach(fruit => {
+//     console.count(fruit)
+// })
+
+// console.countReset('orange')
+
+// oranges.forEach(fruit => {
+//     console.count(fruit)
+// })
+ 
+/* 
+    PRINT THE STACK TRACE
+*/
+
+// const function2 = () => console.trace()
+// const function1 = () => function2()
+
+// function1()
+
+/* 
+   Calculate the time spent
+*/
+
+// const doSomething = () => console.log('test')
+// const measureDoingSomething = () => {
+//     console.time('doSomething fun')
+//     doSomething()
+//     console.timeEnd('doSomething fun')
+// }
+
+// measureDoingSomething()
+
+/* 
+   Calculate the time spent
+*/
+
+// process.stdout.write("Hello \n") 
+
+// console.error("Fatal !!")
+
+/* 
+   Color the output
+*/
+
+// console.log('\x1b[33m%s\x1b[0m','Hello')
+
+const chalk = require('chalk')
+console.log(chalk.yellow("Yellow"))
