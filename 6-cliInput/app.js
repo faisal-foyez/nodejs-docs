2c920456a68a86d04e83cd68ea281e75052af385
faisal64 <faisalislam64@gmail.com>
2022-03-10 19:12:28 +0600

    Practice readline and make a comment section

---
+++ 6-cliInput/app.js
@@ -0,0 +1,9 @@
+// const readline = require('readline').createInterface({
+//     input:process.stdin,
+//     output:process.stdout
+// });
+
+// readline.question(`What is you name`, userName =>{
+//     console.log(`Hi ${userName}`);
+//     readline.close();
+// })
