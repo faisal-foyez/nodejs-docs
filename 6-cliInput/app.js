d5f426a056e5f788624cbc5494e4523b2e4905d7
faisal64 <faisalislam64@gmail.com>
2022-03-10 19:28:41 +0600

    practiced inquirer

--- 6-cliInput/app.js
+++ 6-cliInput/app.js
@@ -1,3 +1,5 @@
+
+/** input readline */
 // const readline = require('readline').createInterface({
 //     input:process.stdin,
 //     output:process.stdout
@@ -7,3 +9,20 @@
 //     console.log(`Hi ${userName}`);
 //     readline.close();
 // })
+///////////////////////
+
+//** reading with inquirer */
+const inquirer  = require('inquirer');
+
+const questions = [
+    {
+        type:'output',
+        name:'fname',
+        message:'What is your name?'
+    }
+]
+
+inquirer.prompt(questions).then(answer =>{
+    console.log(`Hi ${answer['fname']}`);
+})
+////////////////////////////////
