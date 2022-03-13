6f66483de5b673200ab830141f122e70cb8b8ab1
faisal64 <faisalislam64@gmail.com>
2022-03-14 02:15:14 +0600

    Practiced node export

---
+++ nodeExport/app.js
@@ -0,0 +1,11 @@
+// const items = require('./car');
+
+// process.stdout.write(JSON.stringify(items.car));
+
+// const car = require('./car').car;
+
+// process.stdout.write(JSON.stringify(car));
+
+const {car} = require('./car');
+
+process.stdout.write(JSON.stringify(car));
