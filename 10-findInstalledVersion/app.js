380356cf60bac702c650233ffe1575f7c25247de
faisal64 <faisalislam64@gmail.com>
2022-03-14 17:07:34 +0600

    practiced findInstalledVersion

---
+++ 10-findInstalledVersion/app.js
@@ -0,0 +1,25 @@
+/**
+ *  To see the version of all installed npm packages, including their dependencies:
+ *  npm list
+ */ 
+
+/**
+ * npm list -g is the same, but for globally installed packages.
+ */
+
+/**
+ * To get only your top-level packages 
+ * (basically, the ones you told npm to install and you listed in the package.json), 
+ * run npm list --depth=0:
+ */
+
+/**
+ * You can get the version of a specific package by specifying its name:
+ * npm list cowsay
+ */
+
+/**
+ * If you want to see what's the latest available version of the package on the npm repository, 
+ * run npm view [package_name] version
+ * e.g npm view cowsay version
+ */
