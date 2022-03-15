9c0fd66dcc142691b9f275226f0fe5dde627e57a
faisal64 <faisalislam64@gmail.com>
2022-03-15 15:49:43 +0600

    Formatted the comments

--- 9-packageLockJson/app.js
+++ 9-packageLockJson/app.js
@@ -5,13 +5,18 @@
  */  
 
 /**
- * This solves a very specific problem that package.json left unsolved. In package.json you can set which versions you want to upgrade to (patch or minor), using the semver notation, for example:
- *if you write ~0.13.0, you want to only update patch releases: 0.13.1 is ok, but 0.14.0 is not.
- *if you write ^0.13.0, you want to get updates that do not change the leftmost non-zero number: 0.13.1, 0.13.2 and so on. If you write ^1.13.0, you will get patch and minor releases: 1.13.1, 1.14.0 and so on up to 2.0.0 but not 2.0.0.
- *if you write 0.13.0, that is the exact version that will be used, always 
+ * This solves a very specific problem that package.json left unsolved. 
+ * In package.json you can set which versions you want to upgrade to (patch or minor), 
+ * using the semver notation, for example:
+ * if you write ~0.13.0, you want to only update patch releases: 0.13.1 is ok, but 0.14.0 is not.
+ * if you write ^0.13.0, you want to get updates that do not change 
+ * the leftmost non-zero number: 0.13.1, 0.13.2 and so on. 
+ * If you write ^1.13.0, you will get patch and minor releases: 1.13.1, 1.14.0 and so on up to 2.0.0 
+ * but not 2.0.0.
+ * if you write 0.13.0, that is the exact version that will be used, always 
  */
 
  /**
   * The package-lock.json sets your currently installed version of each package in stone, 
   * and npm will use those exact versions when running npm ci
-  */
+  */ 
