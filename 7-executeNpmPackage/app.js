68ffcd800add8e7e18e06575643b9773a536ddd0
faisal64 <faisalislam64@gmail.com>
2022-03-15 15:25:17 +0600

    Added how to execute npm packages

---
+++ 7-executeNpmPackage/app.js
@@ -0,0 +1,11 @@
+/**
+ * const _ = require('lodash');
+ * Now _(loadash can be used in code)
+ */
+
+/**
+ * To execute the executable
+ * You can of course type ./node_modules/.bin/cowsay to run it
+ * but npx, is a much better option
+ * e.g npx cowsay take me out of here
+ */
