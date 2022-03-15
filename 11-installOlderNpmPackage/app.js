83aa6111be9d2dcd05d804bb709e474b6e4c8e91
faisal64 <faisalislam64@gmail.com>
2022-03-15 16:50:41 +0600

    updated cowsay version and practiced installing older node packages

---
+++ 11-installOlderNpmPackage/app.js
@@ -0,0 +1,14 @@
+/**
+ * install previous version
+ * npm install <package>@<version>
+ * e.g npm install cowsay@1.2.0
+ * for global package
+ * npm install -g webpack@4.16.4
+ */
+
+/**
+ * You might also be interested in listing all the previous versions of a package. 
+ * You can do it with 
+ * npm view <package> versions
+ * e.g npm view cowsay versions
+ */
