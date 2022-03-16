42081d9e6ece95c68a95661aabb217e686740d5f
faisal64 <faisalislam64@gmail.com>
2022-03-16 08:13:36 +0600

    practiced local and global packages diff

---
+++ 14-localVsGlobalPackages/app.js
@@ -0,0 +1,23 @@
+/**
+ * In your code you can only require local packages:
+ * e.g require('package-name')
+ */
+
+/**
+ * In general, all packages should be installed locally.
+ */
+
+/**
+ * A package should be installed globally when it provides an executable command 
+ * that you run from the shell (CLI), and it's reused across projects.
+ */
+
+/**
+ * You can also install executable commands locally and run them using npx,
+ *  but some packages are just better installed globally.
+ */
+
+/**
+ * To see the packages installed globally
+ * npm list -g --depth 0
+ */
