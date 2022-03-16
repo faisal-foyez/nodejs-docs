71f7787f3d67f73b5024c38cb6ec53968c084e1e
faisal64 <faisalislam64@gmail.com>
2022-03-16 08:35:55 +0600

    practiced diff between dependencies vs devDependencies

---
+++ 15-dependenciesVsDevdepencencies/app.js
@@ -0,0 +1,14 @@
+/**
+ * The package is automatically listed in the package.json file, 
+ * under the dependencies list (as of npm 5: before you had to manually specify --save).
+ */
+
+/**
+ * When you add the -D flag, or --save-dev, 
+ * you are installing it as a development dependency, which adds it to the devDependencies list.
+ */
+
+/**
+ * You need to set the --production flag (npm install --production) 
+ * to avoid installing those development dependencies.
+ */
