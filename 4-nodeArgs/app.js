71b12ec1e18cf10af01a3b4ea25349d2ebdfd0ac
faisal64 <faisalislam64@gmail.com>
2022-05-11 14:24:57 +0600

    refactored nodeArgs

--- 4-nodeArgs/app.js
+++ 4-nodeArgs/app.js
@@ -8,5 +8,19 @@
 // console.log(args)
 
 const args = require('minimist')(process.argv.slice(2))
-
+console.log(process.argv);
 console.log(args.name)
+
+
+//Input : node app.js --name=jamal  /* because of minimist we need (--) before name
+//Output : jamal
+
+/**
+ * process.argv gives always two arguments one is the location of node module
+ * another is the location of app.js
+ * [
+    '/usr/local/bin/node',
+    '/Users/faisalislam/Desktop/Faisal-Mumbi/nodejs-docs/4-nodeArgs/app.js',
+    '--name=joe'
+   ]
+ */
